fd1e23444256280ab8522cc7c4a1aa0dfa6c03d6
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2020-11-18 14:11:02 +0300

    Add server result in palladium (#28)
    * Add server result in palladium
    
    * Exlude each for before
    
    * Add changes for all files and add hash

--- spec/convertion_by_format/check_open_odt_by_screen_spec.rb
+++ spec/convertion_by_format/check_open_odt_by_screen_spec.rb
@@ -7,22 +7,22 @@ result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
 files = s3.get_files_by_prefix('odt')
 describe 'Convert odt files by convert service' do
   before do
-    @image_size = nil
+    @metadata = nil
   end
   (files - result_sets.map { |result_set| "odt/#{result_set}" }).each do |file_path|
     it File.basename(file_path) do
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=46358' if file_path == 'odt/0100GS3GettingStartedOOo3.odt'
       s3.download_file_by_name(file_path, './files_tmp')
-      response = converter.perform_convert(url: file_uri(file_path), outputtype: 'png')
-      expect(response[:url].nil?).to be_falsey
-      expect(response[:url].empty?).to be_falsey
-      @image_size = ImageHelper.get_image_size(response[:url])
-      expect(@image_size).to be > StaticData::MIN_ODT_IMAGE_SIZE
+      @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: 'png')
+      expect(@metadata[:url].nil?).to be_falsey
+      expect(@metadata[:url].empty?).to be_falsey
+      @metadata[:size] = ImageHelper.get_image_size(@metadata[:url])
+      expect(@metadata[:size]).to be > StaticData::MIN_ODT_IMAGE_SIZE
     end
   end
 
   after :each do |example|
     FileHelper.clear_dir('files_tmp')
-    palladium.add_result_and_log(example, @image_size)
+    palladium.add_result_and_log(example, @metadata[:size], @metadata[:data])
   end
 end
