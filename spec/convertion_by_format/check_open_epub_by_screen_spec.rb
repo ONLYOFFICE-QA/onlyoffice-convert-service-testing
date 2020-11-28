18405a2f4da1e8d4a253a5bb5e7bb1601782a736
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2020-11-28 16:06:28 +0300

    Added  tests for EPUB and FB2 (#42)

---
+++ spec/convertion_by_format/check_open_epub_by_screen_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+FileHelper.clear_dir('files_tmp')
+palladium = PalladiumHelper.new(DocumentServerHelper.get_version, 'Convert EPUB')
+result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
+files = s3.get_files_by_prefix('epub')
+describe 'Convert docx files by convert service' do
+  before do
+    @metadata = nil
+  end
+  (files - result_sets.map { |result_set| "epub``/#{result_set}" }).each do |file_path|
+    it File.basename(file_path) do
+      s3.download_file_by_name(file_path, './files_tmp')
+      @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: 'png')
+      expect(@metadata[:url].nil?).to be_falsey
+      expect(@metadata[:url].empty?).to be_falsey
+      @metadata[:size] = ImageHelper.get_image_size(@metadata[:url])
+      expect(@metadata[:size]).to be > StaticData::MIN_EPUB_IMAGE_SIZE
+    end
+  end
+
+  after :each do |example|
+    FileHelper.clear_dir('files_tmp')
+    palladium.add_result_and_log(example, @metadata[:size], @metadata[:data])
+  end
+end
