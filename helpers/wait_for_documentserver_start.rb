9a670e6e8700370bc9bc8ee8eee5b8661dccf460
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2020-12-14 14:58:49 +0300

    Add timer in console for start service (#67)
    * Add timer in console for start service
    
    * Implement waiting in ruby code
    
    * More simple version

---
+++ helpers/wait_for_documentserver_start.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+puts 'Sleep for waiting documentserver start'
+waiting_time = 120 # Timer limit for documentserver
+waiting_time.times do |i|
+  puts("Waiting for #{i} seconds")
+  sleep 1
+end
+
+puts 'Waiting is end. Run tests'
+
+exec('rspec')
--- wait_for_documentserver_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Sleep for waiting documentserver start"
-sleep 120
-echo "Waiting is end. Run tests"
-exec rspec
