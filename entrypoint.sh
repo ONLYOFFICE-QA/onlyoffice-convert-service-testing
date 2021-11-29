e0db5b82102f1a2e7e5841b45e227168e48eb066
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2021-11-29 14:13:32 +0000

    Fix run current spec file (#418)
    * Fix run current spec file
    
    * Added string check size into CURRENT_SPEC env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 ruby ./helpers/wait_for_documentserver_start.rb
-rspec $CURRENT_SPEC
+
+if [[ -z $CURRENT_SPEC ]]; then
+        echo 'Run all the spec files in their entirety'
+        rspec
+    else
+        echo 'Run the spec file ' $CURRENT_SPEC
+        rspec spec/convertion_by_format/$CURRENT_SPEC
+fi
