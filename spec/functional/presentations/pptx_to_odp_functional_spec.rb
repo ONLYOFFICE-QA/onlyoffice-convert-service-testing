5435f5d75077950185a515b6aa636541d2ebe860
l02 <isrLamb@outlook.com>
2021-12-20 17:59:21 +0300

    Added pptx to odp test

---
+++ spec/functional/presentations/pptx_to_odp_functional_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require './spec/spec_helper'
+FileHelper.clear_dir 'files_tmp'
+palladium = PalladiumHelper.new DocumentServerHelper.get_version, 'Pptx to Odp'
+result_sets = palladium.get_result_sets StaticData::POSITIVE_STATUSES
+files = s3.get_files_by_prefix 'pptx'
+describe 'Convert pptx to odp by convert service' do
+  before do
+    @metadata = nil
+  end
+
+  (files - result_sets.map { |result_set| "pptx/#{result_set}" }).each do |file_path|
+    it File.basename(file_path) do
+      s3.download_file_by_name(file_path, './files_tmp')
+      @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: 'odp')
+      expect(@metadata[:url]).not_to be_nil
+      expect(@metadata[:url]).not_to be_empty
+      @metadata[:file_path] = FileHelper.download_file(@metadata[:url])
+      expect(File).to exist(@metadata[:file_path])
+    end
+  end
+  after do |example|
+    FileHelper.clear_dir('files_tmp')
+    palladium.add_result_and_log(example)
+  end
+end
