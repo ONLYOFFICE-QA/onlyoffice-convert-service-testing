365eedb37fd6cfaf71d8c0d0bcd9303a998aaeb7
l02 <isrLamb@outlook.com>
2021-12-28 16:13:53 +0300

    file exclusion because it is necessary to change the default settings of the document server to work correctly

--- spec/functional/documents/docx_to_rtf_funcrional_spec.rb
+++ spec/functional/documents/docx_to_rtf_funcrional_spec.rb
@@ -12,6 +12,9 @@ describe 'Convert docx to rtf by convert service' do
 
   (files - result_sets.map { |result_set| "docx/#{result_set}" }).each do |file_path|
     it File.basename(file_path) do
+      if file_path == 'docx/Office Open XML Part 4 - Markup Language Reference.docx'
+        pending 'Need to increase the file size limit in document server'
+      end
       s3.download_file_by_name(file_path, './files_tmp')
       @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: 'rtf')
       expect(@metadata[:url]).not_to be_nil
