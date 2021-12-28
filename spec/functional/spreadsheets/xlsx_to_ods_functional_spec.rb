cd56fc3fa56da0d6023bbf6742857a6aae58f548
Nikolai Lambrov <52069739+l8556@users.noreply.github.com>
2021-12-28 17:17:17 +0300

    file exclusion (#454)
    because it is necessary to change the default settings of the document server to work correctly

--- spec/functional/spreadsheets/xlsx_to_ods_functional_spec.rb
+++ spec/functional/spreadsheets/xlsx_to_ods_functional_spec.rb
@@ -12,6 +12,8 @@ describe 'Convert xlsx to ods by convert service' do
 
   (files - result_sets.map { |result_set| "xlsx/#{result_set}" }).each do |file_path|
     it File.basename(file_path) do
+      pending 'Need to increase the conversion timeout' if file_path == 'xlsx/Hasil Treasure 2010 Season 2.xlsx'
+      skip 'File is too big' if file_path == 'xlsx/Smaller50MB.xlsx'
       s3.download_file_by_name(file_path, './files_tmp')
       @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: 'ods')
       expect(@metadata[:url]).not_to be_nil
