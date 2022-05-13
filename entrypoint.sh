3bf0cc8b60819a338a9b6bea1bdfaa66423d3e2a
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2022-05-13 11:22:09 +0000

    Use directory pattern to start the spec (#608)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ if [[ -z $CURRENT_SPEC ]]; then
         echo 'Run all the spec files in their entirety'
         rspec
     else
-        echo 'Run the spec file ' $CURRENT_SPEC
-        rspec spec/conversion_by_format/$CURRENT_SPEC
+        echo 'Run the spec file' $CURRENT_SPEC
+        rspec --pattern "spec/**/$CURRENT_SPEC"
 fi
