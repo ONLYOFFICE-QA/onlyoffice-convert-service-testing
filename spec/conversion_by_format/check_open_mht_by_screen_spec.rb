9d7b854915d502bf64145c476314970a364280aa
l02 <isrLamb@outlook.com>
2022-07-08 12:01:22 +0300

    added functional test to convert mht to png

---
+++ spec/conversion_by_format/check_open_mht_by_screen_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+FileHelper.clear_dir('files_tmp')
+palladium = PalladiumHelper.new(DocumentServerHelper.get_version, 'Convert MHT')
+result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
+files = s3.files_from_folder('mht')
+
+describe 'Convert mht files by convert service' do
+  before do
+    @metadata = nil
+  end
+
+  (files - result_sets.map { |result_set| "mht/#{result_set}" }).each do |file_path|
+    it File.basename(file_path) do
+      s3.download_file_by_name(file_path, './files_tmp')
+      @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: 'png')
+      expect(@metadata[:url]).not_to be_nil
+      expect(@metadata[:url]).not_to be_empty
+      @metadata[:size] = ImageHelper.get_image_size(@metadata[:url])
+      expect(@metadata[:size]).to be > StaticData::MIN_MHT_IMAGE_SIZE
+    end
+  end
+
+  after do |example|
+    FileHelper.clear_dir('files_tmp')
+    palladium.add_result_and_log(example, @metadata[:size], @metadata[:data])
+  end
+end
