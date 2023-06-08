e368ebfcd773782e1794c8ccb952795f999b4f96
l02 <l8556@outlook.com>
2023-06-08 16:43:19 +0300

    Added documents conversion to all formats

---
+++ spec/functional/documents/documents_to_all_formats_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require './spec/spec_helper'
+FileHelper.clear_dir 'files_tmp'
+palladium = PalladiumHelper.new DocumentServerHelper.get_version, 'Documents to All'
+result_sets = palladium.get_result_sets StaticData::POSITIVE_STATUSES
+files = JSON.load_file(File.join(Dir.pwd, 'assets', 'testing_files.json'))['documents']
+output_formats = %w[bmp docm docx docxf dotm dotx epub fb2 gif html jpg odt ott pdf png rtf txt]
+
+describe 'Convert documents to all formats by convert service' do
+  before do
+    @metadata = nil
+  end
+
+  files.each do |file_path|
+    output_formats.each do |format|
+      test_name = "#{File.extname(file_path).delete('.')} to #{format}"
+      next if result_sets.include?(test_name) || File.extname(file_path).delete('.') == format
+
+      it test_name do
+        s3.download_file_by_name(file_path, './files_tmp')
+        result_path = "./files_tmp/#{File.basename(file_path)}.#{format}"
+        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: format)
+        expect(@metadata[:url]).not_to be_nil
+        expect(@metadata[:url]).not_to be_empty
+        FileHelper.download_file(@metadata[:url], result_path)
+        expect(File).to exist(result_path)
+      end
+    end
+  end
+
+  after do |example|
+    FileHelper.clear_dir('files_tmp')
+    palladium.add_result_and_log(example)
+  end
+end
