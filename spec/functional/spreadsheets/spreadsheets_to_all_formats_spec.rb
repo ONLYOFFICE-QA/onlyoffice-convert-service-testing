136933a8bf3312c9763409f7be1a9ababe594c17
l02 <l8556@outlook.com>
2023-06-08 16:44:21 +0300

    Added spreadsheets conversion to all formats

--- spec/functional/spreadsheets/spreadsheets_to_all_formats_spec.rb
+++ spec/functional/spreadsheets/spreadsheets_to_all_formats_spec.rb
@@ -1,30 +1,26 @@
 # frozen_string_literal: true
 
 require './spec/spec_helper'
-
+FileHelper.clear_dir 'files_tmp'
 palladium = PalladiumHelper.new DocumentServerHelper.get_version, 'Spreadsheets to All'
 result_sets = palladium.get_result_sets StaticData::POSITIVE_STATUSES
-files = StaticData::SPREADSHEETS['spreadsheets']
-output_formats = JSON.load_file(File.join(Dir.pwd, 'assets', 'output_formats.json'))
+files = JSON.load_file(File.join(Dir.pwd, 'assets', 'testing_files.json'))['spreadsheets']
+output_formats = %w[bmp	csv	gif	jpg	ods	ots	pdf	png	xlsm	xlsx	xltm	xltx]
 
 describe 'Convert spreadsheets to all formats by convert service' do
   before do
     @metadata = nil
-    @tmp_dir = FileHelper.create_tmp_dir
   end
 
-  files.each do |s3_file_path|
-    input_format = File.extname(s3_file_path).delete('.').to_s
-    out_formats = output_formats.key?(input_format) ? output_formats[input_format] : output_formats['spreadsheets']
-
-    out_formats.each do |out_format|
-      test_name = "#{input_format} to #{out_format}"
-      next if result_sets.include?(test_name) || input_format == out_format
+  files.each do |file_path|
+    output_formats.each do |format|
+      test_name = "#{File.extname(file_path).delete('.')} to #{format}"
+      next if result_sets.include?(test_name) || File.extname(file_path).delete('.') == format
 
       it test_name do
-        file_path = s3.download_file_by_name(s3_file_path, @tmp_dir)
-        result_path = File.join(@tmp_dir, "#{File.basename(s3_file_path)}.#{out_format}")
-        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: out_format)
+        s3.download_file_by_name(file_path, './files_tmp')
+        result_path = "./files_tmp/#{File.basename(file_path)}.#{format}"
+        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: format)
         expect(@metadata[:url]).not_to be_nil
         expect(@metadata[:url]).not_to be_empty
         FileHelper.download_file(@metadata[:url], result_path)
@@ -34,7 +30,7 @@ describe 'Convert spreadsheets to all formats by convert service' do
   end
 
   after do |example|
-    FileUtils.rm_rf(@tmp_dir, secure: true)
+    FileHelper.clear_dir('files_tmp')
     palladium.add_result_and_log(example)
   end
 end
