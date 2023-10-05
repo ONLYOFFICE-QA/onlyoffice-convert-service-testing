a034ca0c8072bdd544f387c5e1d31647300a714f
l02 <l8556@outlook.com>
2023-10-05 13:35:19 +0300

    Added testing of protected document conversion

--- spec/functional/spreadsheets/protected_spreadsheets_to_all_formats_spec.rb
+++ spec/functional/spreadsheets/protected_spreadsheets_to_all_formats_spec.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
 require './spec/spec_helper'
-
+FileHelper.clear_dir 'files_tmp'
 palladium = PalladiumHelper.new DocumentServerHelper.get_version, 'Protected Spreadsheets to All'
 result_sets = palladium.get_result_sets StaticData::POSITIVE_STATUSES
-files = StaticData::SPREADSHEETS['protected_spreadsheets']
+files = JSON.load_file(File.join(Dir.pwd, 'assets', 'testing_files.json'))['protected_spreadsheets']
 output_formats = JSON.load_file(File.join(Dir.pwd, 'assets', 'output_formats.json'))
 
 describe 'Convert protected spreadsheets to all formats by convert service' do
   before do
     @metadata = nil
-    @tmp_dir = FileHelper.create_tmp_dir
   end
 
-  files.each do |s3_file_path|
-    input_format = File.extname(s3_file_path).delete('.').to_s
-    out_formats = output_formats.key?(input_format) ? output_formats[input_format] : output_formats['spreadsheets']
+  files.each do |file_path|
+    input_format = File.extname(file_path).delete('.').to_s
+    formats = output_formats.key?(input_format) ? output_formats[input_format] : output_formats['spreadsheets']
 
-    out_formats.each do |out_format|
-      test_name = "#{input_format} to #{out_format}"
-      next if result_sets.include?(test_name) || input_format == out_format
+    formats.each do |format|
+      test_name = "#{input_format} to #{format}"
+      next if result_sets.include?(test_name) || input_format == format
 
       it test_name do
-        file_path = s3.download_file_by_name(s3_file_path, @tmp_dir)
-        result_path = File.join(@tmp_dir, "#{File.basename(s3_file_path)}.#{out_format}")
-        password = File.basename(s3_file_path).match(/\[pass(\d+)\]/)[1]
-        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: out_format, password: password)
+        s3.download_file_by_name(file_path, './files_tmp')
+        result_path = "./files_tmp/#{File.basename(file_path)}.#{format}"
+        password = File.basename(file_path).match(/\[pass(\d+)\]/)[1]
+        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: format, password: password)
         expect(@metadata[:url]).not_to be_nil
         expect(@metadata[:url]).not_to be_empty
         FileHelper.download_file(@metadata[:url], result_path)
@@ -35,7 +34,7 @@ describe 'Convert protected spreadsheets to all formats by convert service' do
   end
 
   after do |example|
-    FileUtils.rm_rf(@tmp_dir, secure: true)
+    FileHelper.clear_dir('files_tmp')
     palladium.add_result_and_log(example)
   end
 end
