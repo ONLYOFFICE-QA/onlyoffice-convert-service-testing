fd26013dc372eb73ae96836ea01addf6b33f2830
l02 <isrLamb@outlook.com>
2024-02-02 15:27:36 +0200

    minor refactoring

--- spec/functional/documents/documents_to_all_formats_spec.rb
+++ spec/functional/documents/documents_to_all_formats_spec.rb
@@ -15,16 +15,16 @@ describe 'Convert documents to all formats by convert service' do
 
   files.each do |s3_file_path|
     input_format = File.extname(s3_file_path).delete('.').to_s
-    formats = output_formats.key?(input_format) ? output_formats[input_format] : output_formats['documents']
+    out_formats = output_formats.key?(input_format) ? output_formats[input_format] : output_formats['documents']
 
-    formats.each do |format|
-      test_name = "#{input_format} to #{format}"
-      next if result_sets.include?(test_name) || input_format == format
+    out_formats.each do |out_format|
+      test_name = "#{input_format} to #{out_format}"
+      next if result_sets.include?(test_name) || input_format == out_format
 
       it test_name do
         file_path = s3.download_file_by_name(s3_file_path, @tmp_dir)
-        result_path = File.join(@tmp_dir, "#{File.basename(s3_file_path)}.#{format}")
-        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: format)
+        result_path = File.join(@tmp_dir, "#{File.basename(s3_file_path)}.#{out_format}")
+        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: out_format)
         expect(@metadata[:url]).not_to be_nil
         expect(@metadata[:url]).not_to be_empty
         FileHelper.download_file(@metadata[:url], result_path)
--- spec/functional/presentations/protected_presentations_to_all_formats_spec.rb
+++ spec/functional/presentations/protected_presentations_to_all_formats_spec.rb
@@ -15,17 +15,17 @@ describe 'Convert protected presentations to all formats by convert service' do
 
   files.each do |s3_file_path|
     input_format = File.extname(s3_file_path).delete('.').to_s
-    formats = output_formats.key?(input_format) ? output_formats[input_format] : output_formats['presentations']
+    out_formats = output_formats.key?(input_format) ? output_formats[input_format] : output_formats['presentations']
 
-    formats.each do |format|
-      test_name = "#{input_format} to #{format}"
-      next if result_sets.include?(test_name) || input_format == format
+    out_formats.each do |out_format|
+      test_name = "#{input_format} to #{out_format}"
+      next if result_sets.include?(test_name) || input_format == out_format
 
       it test_name do
         file_path = s3.download_file_by_name(s3_file_path, @tmp_dir)
-        result_path = File.join(@tmp_dir, "#{File.basename(s3_file_path)}.#{format}")
+        result_path = File.join(@tmp_dir, "#{File.basename(s3_file_path)}.#{out_format}")
         password = File.basename(s3_file_path).match(/\[pass(\d+)\]/)[1]
-        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: format, password: password)
+        @metadata = converter.perform_convert(url: file_uri(file_path), outputtype: out_format, password: password)
         expect(@metadata[:url]).not_to be_nil
         expect(@metadata[:url]).not_to be_empty
         FileHelper.download_file(@metadata[:url], result_path)
